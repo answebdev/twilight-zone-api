37b080369e058ca5bb9300637a897757917285f0
ANS <36783010+answebdev@users.noreply.github.com>
2021-12-24 10:57:48 -0800

    add endpoints

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(require('./mock/db.js')());
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+
+// console.log('Server is running');
 
 server.listen(port);

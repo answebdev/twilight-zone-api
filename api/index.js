d6d061cff340ace21c01d3d76dd515de6865d2e7
ANS <36783010+answebdev@users.noreply.github.com>
2022-08-31 17:37:06 -0700

    update rate limiting

--- api/index.js
+++ api/index.js
@@ -13,15 +13,15 @@ const compression = require('compression');
 const app = require('express')();
 
 // Rate Limiting
-// const limiter = rateLimit({
-//   windowMs: 24 * 60 * 60 * 1000, // 24 hours
-//   max: 10000,
-// });
-
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 24 * 60 * 60 * 1000, // 24 hours
   max: 10000,
 });
+
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 10000,
+// });
 
 app.use(limiter);
 
